27dda70afd093fe0fa8e4c668c0f205f4643fd0c
indhu-github <indhusworkmail@gmail.com>
2021-09-23 18:07:32 +0530

    js code

--- script.js
+++ script.js
@@ -5,11 +5,33 @@ const loader = document.querySelector(".loader");
 let limit = 3;
 let page = 1;
 
+//Fetch posts from API
 async function getPosts() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   );
 
   const data = await res.json();
+  console.log(data);
   return data;
 }
+
+//Show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+      `;
+    postsContainer.appendChild(postElement);
+  });
+}
+
+showPosts();
